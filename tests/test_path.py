3ad7cc0d17cd07e978e88b987b881ebb198b1729
T.Tian <tian.tian@chem.ethz.ch>
2018-11-16 16:42:13 +0100

    Unittest now working

--- tests/test_path.py
+++ tests/test_path.py
@@ -1,4 +1,10 @@
 import droplet_pressure
 from droplet_pressure.droplet import hello
+import unittest
 
-hello()
+class TestPath(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), 0)
+
+if __name__ == "__main__":
+    unittest.main()
