588a4c76cee3e5f73171732bc1cab80ba572bea7
Tian Tian <lovaulonzezoeta@gmail.com>
2018-11-19 13:40:04 +0100

    Add regression line for elastic modulus

--- samples/pressure_animation.py
+++ samples/pressure_animation.py
@@ -4,6 +4,7 @@ from matplotlib.patches import Arc, Path, PathPatch, Circle, FancyArrowPatch
 from matplotlib.animation import FuncAnimation, FFMpegFileWriter, FFMpegWriter
 import numpy
 from numpy import pi, sin, cos, radians
+from scipy.stats import linregress
 
 def gen_patches(drop, h, resolution=64):
     """Generate patches for droplet at height h
@@ -54,10 +55,11 @@ def gen_patches(drop, h, resolution=64):
     return drop_patch, circle, arr1, arr2, \
            pressure, pr, p_t
 
-def main(vol=1.0e-10,
+def main(vol=3.0e-10,
          theta_t=radians(145),
          theta_b=radians(165),
          total_frames=50,
+         final_frames=4,
          show=False):
     drop = Droplet(initial_volume=vol,
                    theta_t=theta_t,
@@ -81,7 +83,7 @@ def main(vol=1.0e-10,
     ax1.set_title(("Laplace Pressure $p = "
                    " \\gamma (R_{1}^{-1} + R_{2}^{-1})$"))
     ax2.set_xlim(0, 0.25)
-    ax2.set_ylim(0, 500)
+    ax2.set_ylim(0, 300)
     ax2.set_xlabel("Strain")
     ax2.set_ylabel("Stress (Pa)")
     # access patches
@@ -97,25 +99,56 @@ def main(vol=1.0e-10,
                          va="bottom",
                          color="#ffa047")
     patches += [text1, text2]
-    print(patches)
+    # # Additional for plotting the linear regression
+    reg_line, = ax2.plot([], [], "--",
+                        color="#ffae51")
+    text3 = ax2.annotate(xy=(0, 0), s="", color="black")
+    patches += [reg_line, text3]
+    print(len(patches))
+    reg_xy = [None, None]
     
     # update frames
     # limit to 0.25 * h
     def update(i):
         print(i)
-        h = h0 - 0.25 / (total_frames - 1) * i * h0
-        sigma = 1 - h / h0
-        ax1.patches = []
-        dp, c, arr1, arr2, pre, pr, pt = gen_patches(drop, h)
-        patches[:4] = [ax1.add_patch(p) for p in \
-                        [dp, c, arr1, arr2]]
-        l = patches[4]
-        x, y = l.get_data()
-        l.set_data((numpy.concatenate((x, [sigma])),
-                    numpy.concatenate((y, [pre]))))
-        text1, text2 = patches[-2:]
-        text1.set_x(pr[0]); text1.set_y(pr[1]);
-        text2.set_x(pt[0]); text2.set_y(pt[1]);
+        if i < (total_frames - final_frames):
+            # update the patches and pressure only
+            h = h0 - 0.25 / (total_frames - final_frames - 1) * i * h0
+            sigma = 1 - h / h0
+            ax1.patches = []
+            dp, c, arr1, arr2, pre, pr, pt = gen_patches(drop, h)
+            patches[:4] = [ax1.add_patch(p) for p in \
+                           [dp, c, arr1, arr2]]
+            l = patches[4]
+            x, y = l.get_data()
+            l.set_data((numpy.concatenate((x, [sigma])),
+                        numpy.concatenate((y, [pre]))))
+            text1, text2 = patches[5:7]
+            text1.set_x(pr[0]); text1.set_y(pr[1]);
+            text2.set_x(pt[0]); text2.set_y(pt[1]);
+        else:
+            # Update the elastic modulus
+            nonlocal reg_xy     # declare reg_xy not only in this scope
+            if reg_xy[0] is None:
+                l = patches[4]
+                x, y = l.get_data()
+                slope, intercept, *_ = linregress(x[:5], y[:5])
+                reg_xy = [slope, intercept]
+                print(reg_xy)
+            i_frame = i - (total_frames - final_frames) + 1
+            xx = numpy.linspace(0, 0.25, final_frames)
+            yy = reg_xy[0] * xx + reg_xy[1]
+            l2 = patches[7]
+            l2.set_data((xx[:i_frame], yy[:i_frame]))
+            # Add the fitted value to last frame
+            if i == total_frames - 1:
+                text3 = patches[-1]
+                text3.set_x(xx[1]); text3.set_y(yy[1])
+                s = "{:.1e}".format(reg_xy[0]).split("e")
+                base = float(s[0]); p = int(s[1])
+                text3.set_text("Elastic Modulus: {0}$\\times$10$^{{{1}}}$ Pa".format(base, p))
+                text3.set_verticalalignment("top")
+                text3.set_horizontalalignment("left")
         return patches
 
     ani = FuncAnimation(fig, update, frames=total_frames, blit=True)
