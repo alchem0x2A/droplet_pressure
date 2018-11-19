729da2b42cb92be4801c9beaf9a16ab7f894e028
Tian Tian <lovaulonzezoeta@gmail.com>
2018-11-19 12:26:48 +0100

    Fix wrong annotation positions

--- samples/pressure_animation.py
+++ samples/pressure_animation.py
@@ -2,7 +2,6 @@ from droplet_pressure.droplet import Droplet
 import matplotlib.pyplot as plt
 from matplotlib.patches import Arc, Path, PathPatch, Circle, FancyArrowPatch
 from matplotlib.animation import FuncAnimation, FFMpegFileWriter, FFMpegWriter
-from matplotlib.collections import PatchCollection
 import numpy
 from numpy import pi, sin, cos, radians
 
@@ -36,21 +35,24 @@ def gen_patches(drop, h, resolution=64):
     drop_patch.set_alpha(0.5)
     circle = Circle(center_r, radius=r2,
                     ls="--", fill=False,
+                    linewidth=0.5,
                     edgecolor="#ffb4a5")
 
     arr1 = FancyArrowPatch((0, delta_b), (r1, delta_b),
                            mutation_scale=10,
                            linewidth=0,
                            facecolor="#9b9b9b")
-
+    
     arr2 = FancyArrowPatch(center_r, p_t,
                            mutation_scale=10,
                            linewidth=0,
                            facecolor="#ffa047")
 
     pressure = drop.get_delta_stress()
-    print(pressure)
-    return drop_patch, circle, arr1, arr2, pressure
+    pr = (r1, delta_b)
+    print(pr, p_t)
+    return drop_patch, circle, arr1, arr2, \
+           pressure, pr, p_t
 
 def main(vol=1.0e-10,
          theta_t=radians(145),
@@ -68,17 +70,32 @@ def main(vol=1.0e-10,
     ax2 = fig.add_subplot(122)
     ax1.set_ylim(-h0 * 0.05, h0 * 1.25)
     ax1.set_xlim(-h0 * 0.75, h0 * 0.75)
-    ax1.set_axis_off()
+    ax1.set_xlabel("$r$"); ax1.set_ylabel("$z$")
+    ax1.set_xticks([]); ax1.set_yticks([])
+    ax1.axvline(x=0.0, ls="--",
+                color="gray")
+    # ax1.set_axis_off()
     ax1.set_title(("Stress $\\varepsilon = "
                    " \\gamma (R_{1}^{-1} + R_{2}^{-1})$"))
     ax2.set_xlim(0, 0.25)
     ax2.set_ylim(0, 500)
     ax2.set_xlabel("Strain")
     ax2.set_ylabel("Stress (Pa)")
-    dp, c, arr1, arr2, pre = gen_patches(drop, h0)
+    # access patches
+    dp, c, arr1, arr2, pre, pr, pt = gen_patches(drop, h0)
     patches = [ax1.add_patch(p) for p in (dp, c, arr1, arr2)]
     line,  = ax2.plot([0], [pre], "-")
     patches.append(line)
+    text1 = ax1.annotate(xy=pr, s="$R_{1}$",
+                         ha="left", va="bottom",
+                         color="#9b9b9b")
+    text2 = ax1.annotate(xy=pt, s="$R_{2}$",
+                         ha="left",
+                         va="bottom",
+                         color="#ffa047")
+    patches += [text1, text2]
+    print(patches)
+    
     # update frames
     # limit to 0.25 * h
     def update(i):
@@ -86,13 +103,16 @@ def main(vol=1.0e-10,
         h = h0 - 0.25 / (total_frames - 1) * i * h0
         sigma = 1 - h / h0
         ax1.patches = []
-        dp, c, arr1, arr2, pre = gen_patches(drop, h)
-        patches[:-1] = [ax1.add_patch(p) for p in \
+        dp, c, arr1, arr2, pre, pr, pt = gen_patches(drop, h)
+        patches[:4] = [ax1.add_patch(p) for p in \
                         [dp, c, arr1, arr2]]
-        l = patches[-1]
+        l = patches[4]
         x, y = l.get_data()
         l.set_data((numpy.concatenate((x, [sigma])),
                     numpy.concatenate((y, [pre]))))
+        text1, text2 = patches[-2:]
+        text1.set_x(pr[0]); text1.set_y(pr[1]);
+        text2.set_x(pt[0]); text2.set_y(pt[1]);
         return patches
 
     ani = FuncAnimation(fig, update, frames=total_frames, blit=True)
