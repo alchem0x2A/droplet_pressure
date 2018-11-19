090821c0fbcaaf9ab024b641b35dbfdea841c049
T.Tian <tian.tian@chem.ethz.ch>
2018-11-19 18:38:27 +0100

    Successfully added shape keys

--- samples/blender_drop_anim.py
+++ samples/blender_drop_anim.py
@@ -15,12 +15,12 @@ def gen_revolved(verts):
     for i in range(len(verts)):
         x, y, z = verts[i]
         poly.points[i].co = (x, y, z, 1)
-    curvOBJ = bpy.data.objects.new("curveobj", curvData)
-    mod = curvOBJ.modifiers.new("screw", "SCREW")
+    curv_obj = bpy.data.objects.new("curveobj", curvData)
+    mod = curv_obj.modifiers.new("screw", "SCREW")
     mod.use_smooth_shade = True
     mod.steps = 64
     mod.use_normal_flip = True #    
-    return curvOBJ
+    return curv_obj
 
 def gen_verts(params, h0=1.7, resolution=64):
     per, x0, y0, r2, t_b, t_t = params
@@ -36,6 +36,20 @@ def gen_verts(params, h0=1.7, resolution=64):
     # Final point
     verts.append([0, 0, per * h0])
     return verts
+
+def add_shape_keys(curv_obj, param_lists):
+    """Add shape keys for each single frames
+    param_list is already the params without first line
+    """
+    curv_obj.shape_key_add("basis")
+    for i, params in enumerate(param_lists):
+        verts = gen_verts(params)
+        key = curv_obj.shape_key_add("step-{}".format(i))
+        for j, v in enumerate(verts):
+            x, y, z = v
+            key.data[j].co = (x, y, z)
+    return
+    
         
 def read_csv(f_name):
     param_lines = numpy.genfromtxt(f_name, 
@@ -47,11 +61,12 @@ f_name = "/Users/tiantian/polybox/Research/3-(Done)-graphene-F16CuPc-hydrophobic
 z_shift = 0.72
 #verts = [(0, 0, 0), (0, 1, 0), (0, 1, 1), (0, 0, 1)] #test vertices
 param_lines = read_csv(f_name)
-curvOBJ = gen_revolved(gen_verts(param_lines[0]))
+curv_obj = gen_revolved(gen_verts(param_lines[0]))
 scene = bpy.context.scene
-scene.objects.link(curvOBJ)
-curvOBJ.layers[1] = True
-curvOBJ.location = (0, 0, z_shift)
+scene.objects.link(curv_obj)
+curv_obj.layers[1] = True
+add_shape_keys(curv_obj, param_lines[1:])
+#curv_obj.location = (0, 0, z_shift)
 
 
 
