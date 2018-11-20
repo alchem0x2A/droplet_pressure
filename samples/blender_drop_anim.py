94d0a0c2b36e7b6955bb80a411f0a770a05f5a2d
T.Tian <tian.tian@chem.ethz.ch>
2018-11-20 14:59:51 +0100

    Add time function curve

--- samples/blender_drop_anim.py
+++ samples/blender_drop_anim.py
@@ -57,11 +57,61 @@ def add_shape_keys(curv_obj, param_lists):
             key.data[j].co = (x, y, z)
     
     return
+
+def gen_func_curve(total_x, #total length x-direction
+                   total_y, #total length in y-direction
+                   start_frame=1,
+                   period=5, stop=1, repeat=4):
+    """Create the curved path for time function
+    """
+    frames_section = (period + stop) * 2
+    total_frames = frames_section * repeat
+    
+    # velocity for x and y
+    v_x = total_x / total_frames; v_y = total_y / period
+    # frame stop points each section
+    start = 0; tp1 = period; tp2 = tp1 + stop; tp3 = tp2 + period
+    # generate vertex
+    verts = []
+    for i in range(total_frames):
+        x = i * v_x
+        i_section = i % frames_section #Which frame?
+        if i_section <= tp1:
+            y = i_section * v_y
+        elif i_section <= tp2:
+            y = total_y
+        elif i_section <= tp3:
+            y = (tp3 - i_section) * v_y
+        else:
+            y = 0
+        verts.append((0, x, y, 1))
+    print(verts)
+    curvData = bpy.data.curves.new("curve_func", type="CURVE")
+    curvData.dimensions="3D"
+    poly = curvData.splines.new("POLY")
+    poly.points.add(len(verts))
+    for i, v in enumerate(verts):
+        poly.points[i].co = v
+    curv_obj = bpy.data.objects.new("curve_func_obj", curvData)
+    bpy.context.scene.objects.link(curv_obj)
+    curv_obj.data.materials.append(bpy.data.materials["glowing_mater"])
+    #set modifiers
+    mod = curv_obj.modifiers.new("build", "BUILD")
+    mod.frame_start = start_frame; mod.frame_duration = total_frames
+    #set shape
+    curv_obj.data.fill_mode = "FULL"
+    curv_obj.data.bevel_depth = 0.028
+    curv_obj.data.bevel_resolution = 4
+    return curv_obj
+    
+    
+        
     
 def gen_timeline(curv_obj, 
                  other_obj=None, #another object to align
+                 time_curve=False,
                  start_frame=1,
-                 period=5, stop=1, repeat=4):
+                 period=2, stop=4, repeat=6):
     # default
     sk = curv_obj.data.shape_keys
     keys = curv_obj.data.shape_keys.key_blocks
@@ -93,6 +143,11 @@ def gen_timeline(curv_obj,
                                    + curv_obj.dimensions.z
             other_obj.keyframe_insert("location", frame=tp1)
             other_obj.keyframe_insert("location", frame=tp2)
+    if time_curve is True:
+        curv_func_obj = gen_func_curve(total_x=6.0,
+                                       total_y=2.0,
+                                       start_frame=start_frame,
+                                       period=period, stop=stop, repeat=repeat)
     return
         
     
@@ -112,6 +167,7 @@ curv_obj.data.materials.append(bpy.data.materials["Hg"])
 add_shape_keys(curv_obj, param_lines[1:])
 curv_obj.location = (0, 0, z_shift)
 gen_timeline(curv_obj, other_obj=bpy.data.objects["drain"],
+             time_curve=True,
              start_frame=22)
 
 
